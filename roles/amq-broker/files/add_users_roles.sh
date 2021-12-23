098a8a0eab9872924bf5efce288ff0636e9e687b
mcaimi <mcaimi@redhat.com>
2021-12-23 14:41:42 +0100

    Updated amq-broker role

--- roles/amq-broker/files/add_users_roles.sh
+++ roles/amq-broker/files/add_users_roles.sh
@@ -4,9 +4,11 @@ ARTEMIS_INSTANCE_HOME=${1}
 USER=${2}
 PASS=${3}
 ROLE=${4}
+ADMIN_USER=${5}
+ADMIN_PASS=${6}
 
 # add users and roles
-$ARTEMIS_INSTANCE_HOME/bin/artemis user add --user $USER --password $PASS --role $ROLE 2>&1 > /dev/null
+$ARTEMIS_INSTANCE_HOME/bin/artemis user add --user-command-user $USER --user-command-password $PASS --role $ROLE --user $ADMIN_USER --pass $ADMIN_PASS 2>&1 > /dev/null
 if [[ $? -ne 0 ]]; then
   echo -n ALREADY_EXISTS
   exit 0
